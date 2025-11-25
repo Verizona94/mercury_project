9703d76273a69c972255235d6b91b4439d033a3c
Sergey Arslanov <Verizona94@yandex.ru>
2025-11-25 15:49:10 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from .models import Course, Lesson
 
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 1
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline] # Позволяет добавлять уроки прямо внутри курса
+    list_display = ('title', 'price', 'created_at')
